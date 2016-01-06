56c75fa3f7769eb4c10ac7505c2a5c9e6ad0f8c0
Miyamoto <scotsdowne_road@yahoo.co.jp>
2016-01-06 21:58:47 +0900

    first commit

---
+++ access_program_memory.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   access_program_memory.h
+ * Author: Naoy_Miyamoto
+ *
+ * Created on 2016/01/06, 21:51
+ */
+
+#ifndef ACCESS_PROGRAM_MEMORY_H
+#define	ACCESS_PROGRAM_MEMORY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ACCESS_PROGRAM_MEMORY_H */
+
